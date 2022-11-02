2a3e41068c06a8a32337cfc80ee16bb06a264d72
Daria Zaitseva <daria.zaitseva@easybrain.com>
2022-11-03 00:04:14 +0300

    add localization

---
+++ src/js/localization.js
@@ -0,0 +1,45 @@
+var localization = {
+    langs: ['en', 'ru'],
+
+    loading: {
+        en: 'Loading...',
+        ru: 'Загрузка...'
+    },
+    movesleft: {
+        en: 'Moves left: ',
+        ru: 'Ходов осталось: '
+    },
+    score: {
+        en: 'Score: ',
+        ru: 'Очки: '
+    },
+    goal: {
+        en: 'Goal: ',
+        ru: 'Цель: '
+    },
+    wintitle: {
+        en: 'Good job!',
+        ru: 'Отличная работа!'
+    },
+    superwintitle: {
+        en: 'You\'ve beat the game!',
+        ru: 'Игра пройдена!'
+    },
+    losetitle: {
+        en: 'Oops!',
+        ru: 'Упс!'
+    },
+    nextlevel: {
+        en: 'Next Level',
+        ru: 'Дальше'
+    },
+    retry: {
+        en: 'Retry',
+        ru: 'Переиграть уровень'
+    },
+    restart: {
+        en: 'Restart',
+        ru: 'Начать заново'
+    },
+};
+window.localization = localization;
